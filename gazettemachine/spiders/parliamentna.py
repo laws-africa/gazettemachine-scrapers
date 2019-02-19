3b587aba68eb8783a0913f9d261ba02a241a7837
Greg Kempe <greg@kempe.net>
2019-02-19 16:12:35 +0200

    Initial commit

---
+++ gazettemachine/spiders/parliamentna.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class ParliamentnaSpider(scrapy.Spider):
+    name = 'parliamentna'
+    allowed_domains = ['laws.parliament.na']
+    start_urls = ['https://laws.parliament.na/gazettes/namibia-government-gazettes.php?id=1']
+
+    def parse(self, response):
+        for href in response.css('table.page-content-table td:first-child a::attr(href)'):
+            yield response.follow(href, self.parse_listing)
+
+    def parse_listing(self, response):
+        for href in response.css('table.page-content-table td:first-child a::attr(href)'):
+            yield {
+                'jurisdiction': 'na',
+                'href': href.get(),
+            }
