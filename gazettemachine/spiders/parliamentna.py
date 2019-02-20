0e62bce1db95ac0ef7674b86611a83f6192f133f
Greg Kempe <greg@kempe.net>
2019-02-20 20:50:52 +0200

    Use paginated URLs

--- gazettemachine/spiders/parliamentna.py
+++ gazettemachine/spiders/parliamentna.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import urllib.parse as urlparse
 import scrapy
 
 
@@ -12,8 +13,27 @@ class ParliamentnaSpider(scrapy.Spider):
 
     def parse(self, response):
         for href in response.css('table.page-content-table td:first-child a::attr(href)'):
-            yield response.follow(href, self.parse_listing)
+            yield response.follow(href, self.parse_main_listing)
 
-    def parse_listing(self, response):
+    def parse_main_listing(self, response):
+        if response.css('#loadmore-gazette'):
+            parsed = urlparse.urlparse(response.url)
+            year = int(urlparse.parse_qs(parsed.query)['y'][0])
+            yield response.follow('https://laws.parliament.na/ajax/ajax.fetchGazettes.php?id=1&y=%s&page=2' % year,
+                                  self.parse_partial_listing,
+                                  meta={'year': year, 'page': 2})
+
         for href in response.css('table.page-content-table td:first-child a::attr(href)'):
+            yield GazetteMachineItem(jurisdiction='na', url=href.extract())
+
+    def parse_partial_listing(self, response):
+        if response.body.strip():
+            # probably another page
+            page = response.meta['page'] + 1
+            year = response.meta['year']
+            yield response.follow('https://laws.parliament.na/ajax/ajax.fetchGazettes.php?id=1&y=%s&page=%s' % (year, page),
+                                  self.parse_partial_listing,
+                                  meta={'year': response.meta['year'], 'page': page})
+
+        for href in response.css('td:first-child a::attr(href)'):
             yield GazetteMachineItem(jurisdiction='na', url=href.extract())
