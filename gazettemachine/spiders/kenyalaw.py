410856cf92d08b4884aa94d57f72f6383112f07a
Greg Kempe <greg@kempe.net>
2020-01-24 14:17:58 +0200

    Kenyalaw gazette spider

---
+++ gazettemachine/spiders/kenyalaw.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import urllib.parse as urlparse
+import scrapy
+import datetime
+
+
+from gazettemachine.items import GazetteMachineItem
+
+
+class KenyalawSpider(scrapy.Spider):
+    name = 'kenyalaw'
+    allowed_domains = ['kenyalaw.org', 'www.kenyalaw.org']
+
+    def start_requests(self):
+        for year in range(2020, datetime.date.today().year + 1):
+            yield scrapy.Request(f'http://www.kenyalaw.org/kenya_gazette/gazette/year/{year}/', self.parse)
+
+    def parse(self, response):
+        for href in response.css('.page-content .gazette-content table td:nth-child(2) a::attr(href)'):
+            yield response.follow(href, self.parse_gazette_page)
+
+    def parse_gazette_page(self, response):
+        for href in response.css('.gazette-content .sd a::attr(href)'):
+            href = href.get()
+            if href.lower().endswith('.pdf'):
+                yield GazetteMachineItem(jurisdiction='ke', url=href)
