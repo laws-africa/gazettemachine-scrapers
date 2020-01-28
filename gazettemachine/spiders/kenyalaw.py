7f2922b33ca559316741d8fa0566217274e28877
Greg Kempe <greg@kempe.net>
2020-01-28 08:58:34 +0200

    Parse KGS listing pages

--- gazettemachine/spiders/kenyalaw.py
+++ gazettemachine/spiders/kenyalaw.py
@@ -12,15 +12,27 @@ class KenyalawSpider(scrapy.Spider):
     allowed_domains = ['kenyalaw.org', 'www.kenyalaw.org']
 
     def start_requests(self):
+        # KG
         for year in range(2019, datetime.date.today().year + 1):
-            yield scrapy.Request(f'http://www.kenyalaw.org/kenya_gazette/gazette/year/{year}/', self.parse)
+            yield scrapy.Request(f'http://www.kenyalaw.org/kenya_gazette/gazette/year/{year}/', self.parse_kg_listing)
 
-    def parse(self, response):
+        # KGS
+        for nid in ['9291', '4422', '5194', '5995', '6821', '7936']:
+            yield scrapy.Request(f'http://kenyalaw.org/kl/index.php?id={nid}', self.parse_kgs_listing)
+
+    def parse_kg_listing(self, response):
         for href in response.css('.page-content .gazette-content table td:nth-child(2) a::attr(href)'):
-            yield response.follow(href, self.parse_gazette_page)
+            yield response.follow(href, self.parse_kg_page)
 
-    def parse_gazette_page(self, response):
+    def parse_kg_page(self, response):
         for href in response.css('.gazette-content .sd a::attr(href)'):
+            href = href.get()
+            if href.lower().endswith('.pdf'):
+                url = response.urljoin(href)
+                yield GazetteMachineItem(jurisdiction='ke', url=url)
+
+    def parse_kgs_listing(self, response):
+        for href in response.css('.page-content .bodytext a::attr(href)'):
             href = href.get()
             if href.lower().endswith('.pdf'):
                 url = response.urljoin(href)
