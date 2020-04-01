87653b4908defab37576cdcf171c4f06473469ca
Greg Kempe <greg@kempe.net>
2020-04-01 09:45:55 +0200

    cookies

--- gazettemachine/spiders/govrw.py
+++ gazettemachine/spiders/govrw.py
@@ -12,6 +12,11 @@ class GovRWSpider(scrapy.Spider):
     allowed_domains = ['minijust.gov.rw', 'www.minijust.gov.rw']
     start_urls = ['https://www.minijust.gov.rw/index.php?id=133']
 
+    def start_requests(self, *args, **kwargs):
+        for request in super(GovRWSpider, self).start_requests(*args, **kwargs):
+            request.cookies['_accessKey2'] = '2uzM7wFZZENjveYWRx5OL2rAy2Mp33zU'
+            yield request
+
     def parse(self, response):
         # paginator
         for href in response.css('.page-navigation .pagination li a::attr(href)'):
