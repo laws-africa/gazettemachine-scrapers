d0e5f572b1d4cc0b0d123d031a3218037fdd557c
Greg Kempe <greg@kempe.net>
2021-08-19 14:42:21 +0200

    Add joradp.dz scraper

---
+++ gazettemachine/spiders/joradpdz.py
@@ -0,0 +1,29 @@
+import scrapy
+import datetime
+
+
+from gazettemachine.items import GazetteMachineItem
+
+
+class JoradpdzScraper(scrapy.Spider):
+    name = 'joradpdz'
+    allowed_domains = ['joradp.dz']
+
+    def start_requests(self):
+        # years
+        # TODO: once deployed and run once, only do this year and next year
+        current_year = datetime.date.today().year
+        for year in range(1962, current_year + 2):
+            yield scrapy.Request(f'https://www.joradp.dz/JRN/ZF{year}.htm', self.parse_listing,
+                                 cb_kwargs={'year': year})
+
+    def parse_listing(self, response, year):
+        for opt in response.css('form[name="zFrm2"] select[name="znjo"] > option[value]'):
+            try:
+                num = int(opt.attrib['value'])
+            except ValueError:
+                continue
+
+            yield GazetteMachineItem(
+                jurisdiction='dz',
+                url=f'https://www.joradp.dz/FTP/JO-FRANCAIS/{year}/F{year}{num:03}.pdf')
