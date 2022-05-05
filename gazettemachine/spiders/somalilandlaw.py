f65c18a991de51705419ef1b0b3895f5ee1f5fe6
Robley G <robleyadrian@gmail.com>
2022-05-05 12:01:37 +0300

    add somaliland scraper

---
+++ gazettemachine/spiders/somalilandlaw.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+from gazettemachine.items import GazetteMachineItem
+
+
+class SomalilandLawSpider(scrapy.Spider):
+    name = 'somalilandlaw'
+    allowed_domains = ['somalilandlaw.com', 'www.somalilandlaw.com']
+    start_urls = ['http://www.somalilandlaw.com/somaliland_official_gazette.html']
+
+    def parse(self, response):
+        # all gazettes are on the same page and end in .pdf
+        for href in response.css('a::attr(href)'):
+            url = href.get()
+            if url.lower().endswith('.pdf'):
+                url = response.urljoin(url)
+                yield GazetteMachineItem(jurisdiction='so', url=url)
+
