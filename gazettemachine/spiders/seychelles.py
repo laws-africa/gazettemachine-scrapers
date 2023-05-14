60ce9c34244148d79f7bbd1412bee8bf5cf3c919
Greg Kempe <greg@kempe.net>
2023-05-14 09:36:50 +0200

    scrape SC bills, acts and SIs

--- gazettemachine/spiders/seychelles.py
+++ gazettemachine/spiders/seychelles.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import scrapy
 import datetime
 
@@ -10,8 +9,11 @@ class SeychellesSpider(scrapy.Spider):
     allowed_domains = ['gazette.sc', 'www.gazette.sc']
 
     def start_requests(self):
-        for year in range(2022, datetime.date.today().year + 1):
+        for year in range(2023, datetime.date.today().year + 1):
             yield scrapy.Request(f'https://www.gazette.sc/v/gazette/{year}', self.parse)
+            yield scrapy.Request(f'https://www.gazette.sc/v/act/{year}', self.parse)
+            yield scrapy.Request(f'https://www.gazette.sc/v/si/{year}', self.parse)
+            yield scrapy.Request(f'https://www.gazette.sc/v/bill/{year}', self.parse)
 
     def parse(self, response):
         for href in response.css('div.views-infinite-scroll-content-wrapper div.views-field-nothing a::attr(href)'):
