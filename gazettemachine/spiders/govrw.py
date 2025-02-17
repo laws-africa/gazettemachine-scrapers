cbc40af04e83a53adb6c1e6dcd508c4a938933f1
Greg Kempe <greg@kempe.net>
2025-02-17 10:19:01 +0200

    gov.rw now requires a user agent

--- gazettemachine/spiders/govrw.py
+++ gazettemachine/spiders/govrw.py
@@ -8,6 +8,9 @@ class GovRWSpider(scrapy.Spider):
     name = 'govrw'
     allowed_domains = ['minijust.gov.rw', 'www.minijust.gov.rw']
     start_urls = ['https://www.minijust.gov.rw/official-gazette']
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    }
 
     def parse(self, response):
         # year, month and gazette listing pages all have the same format
